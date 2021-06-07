d15667d599e6303b35602c19299cf93c492de900
Tony Kharioki <antolisah@gmail.com>
2021-06-07 05:06:12 +0300

    user list and admin ui

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const { Keystone } = require('@keystonejs/keystone');
 const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
 const { GraphQLApp } = require('@keystonejs/app-graphql');
-const { Text } = require('@keystonejs/fields');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
 
 const TodoSchema = require('./lists/Todo.js');
+const UserSchema = require('./lists/User.js');
 
 const keystone = new Keystone({
   adapter: new MongooseAdapter({ mongoUri: 'mongodb://localhost/keystone' }),
 });
 
 keystone.createList('Todo', TodoSchema);
-
+keystone.createList('User', UserSchema);
 
 module.exports = {
   keystone,
-  apps: [new GraphQLApp()],
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({
+      adminPath: '/admin',
+    })
+  ],
 };
