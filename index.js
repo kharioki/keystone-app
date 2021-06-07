40367037c3e7c6bd91f74aa4ab543420499b38ce
Tony Kharioki <antolisah@gmail.com>
2021-06-07 04:48:10 +0300

    initializing keystone and graphql instance

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { Text } = require('@keystonejs/fields');
+
+const keystone = new Keystone({
+  adapter: new MongooseAdapter({ mongoUri: 'mongodb://localhost/keystone' }),
+});
+
+keystone.createList('Todo', {
+  fields: {
+    name: { type: Text },
+  },
+});
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp()],
+};
